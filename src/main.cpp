992b860c532c8aa3d537b54bb294679010a75c96
Liam Metzger <liam.metzger@bluewin.ch>
2025-04-03 20:48:03 +0200

    fix: Fix some left over code
    Switch code to use the new Logger
    Remove unused temporary code
    
    Signed-off-by: Liam Metzger <liam.metzger@bluewin.ch>

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
 #include <cerrno>
 #include <csignal>
 #include <cstdlib>
-#include <iostream>
 
 #include "events/EpollPoller.hpp"
 #include "events/SelectPoller.hpp"
-#include "webserver/WebServer.hpp"
+#include "logging/Logger.hpp"
 #include "signal/SignalHandler.hpp"
+#include "webserver/WebServer.hpp"
 
 constexpr int PORT = 8080;
-
-// void signalHandler(int signal)
-// {
-// 	std::cout << "Received signal " << signal << ". Stopping server..." << std::endl;
-// }
 
 int main()
 {
 	SignalHandler& signalHandler = *SignalHandler::getInstance();
-
-	// std::signal(SIGINT, signalHandler);
 
 	try
 	{
@@ -32,26 +25,30 @@ int main()
 		WebServer server("0.0.0.0", PORT, std::move(poller));
 		signalHandler.bind(SIGINT, [&server](int) { server.stop(); });
 
-		server.registerRoute(HttpMethod::GET, "/", [](const HttpRequest&, HttpResponse& response) {
-			response.headers["Content-Type"] = "text/plain";
-			response.body = "Hello, world!";
+		server.registerRoute(HttpMethod::GET, "/",
+							 [](const HttpRequest&, HttpResponse& response)
+							 {
+								 response.headers["Content-Type"] = "text/plain";
+								 response.body = "Hello, world!";
 
-			return response;
-		});
+								 return response;
+							 });
 
-		server.registerRoute(HttpMethod::POST, "/echo", [](const HttpRequest& request, HttpResponse& response) {
-			if (request.headers.contains("Content-Type"))
-			{
-				response.headers["Content-Type"] = request.headers.at("Content-Type");
-			}
-			response.body = request.body;
-		});
+		server.registerRoute(HttpMethod::POST, "/echo",
+							 [](const HttpRequest& request, HttpResponse& response)
+							 {
+								 if (request.headers.contains("Content-Type"))
+								 {
+									 response.headers["Content-Type"] = request.headers.at("Content-Type");
+								 }
+								 response.body = request.body;
+							 });
 
 		server.run();
 	}
 	catch (const std::exception& e)
 	{
-		std::cerr << "Server error: " << e.what() << std::endl;
+		Logger::error("Server error: {}", e.what());
 		return EXIT_FAILURE;
 	}
 
