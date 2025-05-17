cfd128eb0d1aa44f51aeb27dd8aee2691f93208d
Liam Metzger <liam.metzger@bluewin.ch>
2025-05-17 23:15:46 +0200

    style: Remove unnecessary things
    Signed-off-by: Liam Metzger <liam.metzger@bluewin.ch>

--- include/events/EventPoller.hpp
+++ include/events/EventPoller.hpp
@@ -8,12 +8,6 @@ public:
 	EventPoller() = default;
 	virtual ~EventPoller() = default;
 
-	EventPoller(const EventPoller&) = delete;
-	EventPoller& operator=(const EventPoller&) = delete;
-
-	EventPoller(EventPoller&&) = delete;
-	EventPoller& operator=(EventPoller&&) = delete;
-
 	virtual void add(int fd) = 0;
 	virtual void remove(int fd) = 0;
 
